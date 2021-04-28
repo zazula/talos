6e0f15a1c9d1fd75689bfa9aeae6c0623ac82b52
Ralph Zazula <zazula@me.com>
2021-04-28 00:58:53 -0700

    test of GPU allocation

--- talos/scan/scan_run.py
+++ talos/scan/scan_run.py
@@ -22,15 +22,23 @@ def scan_run(self):
     processes = []
     total = len(self.param_object.param_index)
     count = 0
+    gpu_tickets = [0,0,0,0,0,0,0,0]
     while True:
 
         if self.use_multiprocessing:
             while len(processes) >= self.max_processes:
                 done = multiprocessing.connection.wait([x.sentinel for x in processes])
                 for p in filter(lambda x: x.sentinel in done, processes):
+                    gpu_id = -1
+                    try:
+                        gpu_id = getattr(p, 'gpu_id')
+                    except:
+                        pass
+                    if gpu_id != -1:
+                        gpu_tickets[gpu_id] = 0
                     processes.remove(p)
                     count += 1
-                    print(f"completed {count} of {total} runs ({(count * 100.0) / total :0.2f}%)")
+                    print(f"completed {count} of {total} runs ({(100.0 * count) / total : 0.2f}%)")
                     self.pbar.update(1)
                 
         # get the parameters
@@ -43,7 +51,18 @@ def scan_run(self):
         # otherwise proceed with next permutation
         from .scan_round import scan_round
         if self.use_multiprocessing:
+            # allocate a GPU
+            gpu_id = 0
+            try:
+                gpu_id = gpu_tickets.index(0)
+                gpu_tickets[gpu_id] = 1
+            except:
+                # no GPU available
+                gpu_id = -1
+                pass
             p = Process(target=scan_round, args=(self,))
+            self.round_params['GPU_ID'] = gpu_id
+            setattr(p, 'gpu_id', gpu_id)
             processes.append(p)
             p.start()
         else:
@@ -54,9 +73,16 @@ def scan_run(self):
         while len(processes) > 0:
             done = multiprocessing.connection.wait([x.sentinel for x in processes])
             for p in filter(lambda x: x.sentinel in done, processes):
+                gpu_id = -1
+                try:
+                    gpu_id = getattr(p, 'gpu_id')
+                except:
+                    pass
                 processes.remove(p)
+                if gpu_id != -1:
+                    gpu_tickets[gpu_id] = 0
                 count += 1
-                print(f"completed {count} of {total} runs ({(count * 100.0) / total : 0.2f}%)")
+                print(f"completed {count} of {total} runs ({(100.0 * count) / total : 0.2f}%)")
                 self.pbar.update(1)
 
     # close progress bar before finishing
